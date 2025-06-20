15e1c97288880233810c4fe7ddce5e68469c2873
Insitektalay <tm30junglehouse@example.com>
2025-06-20 10:32:59 +0100

    Initial Commit

---
+++ ParentGuidance/ParentGuidanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParentGuidanceApp.swift
+//  ParentGuidance
+//
+//  Created by alex kerss on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ParentGuidanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
